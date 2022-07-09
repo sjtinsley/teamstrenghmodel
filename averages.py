66c1ea0bb8585103abbf29380349bacb0e2844e0
Simon Tinsley <45486136+sjtinsley@users.noreply.github.com>
2022-07-09 15:39:13 +0100

    Fixing indentation error

--- averages.py
+++ averages.py
@@ -1,10 +1,10 @@
 from teams2223 import *
 import numpy as np
 
-    teams = [arsenal, astonvilla, bournemouth, brentford, brighton, chelsea, palace, everton, fulham, leeds, leicester,
+teams = [arsenal, astonvilla, bournemouth, brentford, brighton, chelsea, palace, everton, fulham, leeds, leicester,
              liverpool, mancity, manutd, newcastle, forest, southampton, tottenham, westham, wolves]
 
-    goalsavg = sum(t.attack for t in teams) / 20
-    finattavg = sum(t.finatt for t in teams) / 20
-    findefavg = sum(t.findef for t in teams) / 20
-    npgavg = sum(t.npxgattack for t in teams) / 20
+goalsavg = sum(t.attack for t in teams) / 20
+finattavg = sum(t.finatt for t in teams) / 20
+findefavg = sum(t.findef for t in teams) / 20
+npgavg = sum(t.npxgattack for t in teams) / 20
